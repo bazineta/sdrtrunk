05b3b3bdec321458b94b9a2ee720899b957c45f8
snepple <25994154+snepple@users.noreply.github.com>
2026-04-22 18:12:00 +0000

    Fix merge conflicts for System Health feature

---
+++ fix_logs_panel_conflict.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i '/<<<<<<< HEAD/d' src/main/java/io/github/dsheirer/gui/LogsPanel.java
+sed -i '/=======/d' src/main/java/io/github/dsheirer/gui/LogsPanel.java
+sed -i '/>>>>>>> origin\/master/d' src/main/java/io/github/dsheirer/gui/LogsPanel.java
